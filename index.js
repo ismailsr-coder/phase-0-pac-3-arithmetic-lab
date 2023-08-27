65b2ad28246e1eb9d45f7f2cacc438ce49c9992d
ISMAIL <ismailsabbr12@gmail.com>
2023-08-26 21:28:11 -0600

    Completed

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+add(30, 30);
+function subtract(a, b) {
+    return a - b;
+}
+subtract(30,30);
+function multiply(a, b) {
+    return a * b;
+}
+multiply(30,30);
+function divide(a, b) {
+    return a / b;
+}
+divide(30,30);
+function increment(n) {
+    n++;
+    return n;
+}
+function decrement(n) {
+    n--;
+    return n;
+}
+increment(30);
+dencrement(30);
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
